3691287c5f3666d2c7cb016b667e6fa49c8d6b71
ciordan <Catalin.Iordan@endava.com>
2020-08-06 19:57:38 +0300

    Initial commit

---
+++ src/main/java/com/online/compiler/runnerapi/JavaServiceApiApplication.java
@@ -0,0 +1,14 @@
+package com.online.compiler.runnerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+
+@SpringBootApplication
+public class JavaServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServiceApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/online/compiler/runnerapi/JavaServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.compiler.runnerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
