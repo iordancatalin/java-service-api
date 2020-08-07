22ef038391b51bcfab66a0d9170d83798b7bd6db
ciordan <Catalin.Iordan@endava.com>
2020-08-07 13:24:00 +0300

    Feature #38 Enable cors for all origins, method and headers.

--- src/main/java/com/online/compiler/runnerapi/JavaServiceApiApplication.java
+++ src/main/java/com/online/compiler/runnerapi/JavaServiceApiApplication.java
@@ -3,8 +3,6 @@ package com.online.compiler.runnerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.*;
-
 @SpringBootApplication
 public class JavaServiceApiApplication {
 
