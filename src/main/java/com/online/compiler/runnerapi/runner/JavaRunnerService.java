3cb2caf2db467a789b5e97a321d6213eb020bcc8
ciordan <Catalin.Iordan@endava.com>
2020-08-08 20:44:39 +0300

    Improvement #53
    Java-service-api creates the output file and send the path to java-runner as the second argument.

--- src/main/java/com/online/compiler/runnerapi/runner/JavaRunnerService.java
+++ src/main/java/com/online/compiler/runnerapi/runner/JavaRunnerService.java
@@ -6,9 +6,12 @@ import com.online.compiler.runnerapi.runner.exception.CodeNotCompilableException
 import com.online.compiler.runnerapi.runner.executor.CodeExecutor;
 import com.online.compiler.runnerapi.runner.model.ExecutionLog;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -16,6 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.online.compiler.runnerapi.common.Constants.*;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class JavaRunnerService {
@@ -48,7 +52,21 @@ public class JavaRunnerService {
         final var pathToClassDirectory = COMPILED_CLASSES_DIRECTORY + "/" + classDirectoryName;
         final var outputFilePath = pathToClassDirectory + "/" + OUTPUT_FILE_NAME;
 
+        createOutputFile(outputFilePath);
+
         return codeExecutor.executeCompiledCode(pathToClassDirectory, outputFilePath, FILE_NAME);
+    }
+
+    private void createOutputFile(String outputFilePath) {
+        final var file = new File(outputFilePath);
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                log.error(e);
+            }
+        }
     }
 
     private Path createJavaFilePath(String parentDirectoryName) {
