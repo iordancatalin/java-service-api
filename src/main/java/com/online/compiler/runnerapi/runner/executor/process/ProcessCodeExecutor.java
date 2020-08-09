87fea2337bf252af96b8b0d1eae4d191855921ee
ciordan <Catalin.Iordan@endava.com>
2020-08-09 15:42:41 +0300

    Feature #56 Log errors when the processBuilder from class ProcessCodeExecutor (method executeCompiledCode) exits with status diff then 0

--- src/main/java/com/online/compiler/runnerapi/runner/executor/process/ProcessCodeExecutor.java
+++ src/main/java/com/online/compiler/runnerapi/runner/executor/process/ProcessCodeExecutor.java
@@ -3,50 +3,76 @@ package com.online.compiler.runnerapi.runner.executor.process;
 import com.online.compiler.runnerapi.runner.executor.CodeExecutor;
 import com.online.compiler.runnerapi.runner.model.ExecutionLog;
 import com.online.compiler.runnerapi.runner.model.LogLevelEnum;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Base64;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Log4j2
 @Service
-@RequiredArgsConstructor
 public class ProcessCodeExecutor implements CodeExecutor {
 
-    private static final String JAVA_RUNNER_PATH = "C:/projects/disertatie/jars";
-    private static final String JAVA_RUNNER_JAR_NAME = "java-runner-1.0-SNAPSHOT.jar";
-    private static final String SEPARATOR = "\\.";
+    private final String javaRunnerPath;
+    private final String javaRunnerJarName;
+    private final String separator;
+
+    public ProcessCodeExecutor(@Value("${java.runner.path}") String javaRunnerPath,
+                               @Value("${java.runner.jar.name}") String javaRunnerJarName,
+                               @Value("${java.runner.content.separator}") String separator) {
+        this.javaRunnerPath = javaRunnerPath;
+        this.javaRunnerJarName = javaRunnerJarName;
+        this.separator = separator;
+    }
 
     @Override
-    public List<ExecutionLog> executeCompiledCode(String pathToClassDirectory,
-                                                  String outputFilePath,
-                                                  String className) {
+    public CompletableFuture<List<ExecutionLog>> executeCompiledCode(String pathToClassDirectory,
+                                                                     String outputFilePath,
+                                                                     String className) {
         try {
             final var command = buildRunCommand(pathToClassDirectory, outputFilePath, className);
 
             final var processBuilder = new ProcessBuilder(command);
-            processBuilder.directory(new File(JAVA_RUNNER_PATH));
+            processBuilder.directory(new File(javaRunnerPath));
 
             final var process = processBuilder.start();
-            process.waitFor();
+            return process.onExit()
+                    .thenApply(processFinished -> {
+                        if (processFinished.exitValue() != 0) {
+                            logErrorStream(processFinished.getErrorStream());
+                        }
+                        return getLogsFromOutputFile(outputFilePath);
+                    });
 
-            try (Stream<String> stream = Files.lines(Path.of(outputFilePath))) {
-                return stream.map(this::buildExecutionLog)
-                        .collect(Collectors.toList());
-            }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
+            log.error(e);
+        }
+
+        return CompletableFuture.supplyAsync(List::of);
+    }
+
+    private List<ExecutionLog> getLogsFromOutputFile(String outputFilePath) {
+        try (Stream<String> stream = Files.lines(Path.of(outputFilePath))) {
+            return stream.map(this::buildExecutionLog)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
             log.error(e);
         }
 
         return List.of();
+    }
+
+    private void logErrorStream(InputStream errorStream) {
+        try (var stream = new BufferedReader(new InputStreamReader(errorStream)).lines()) {
+            stream.forEach(log::error);
+        }
     }
 
     private ExecutionLog buildExecutionLog(String encodedLog) {
@@ -54,7 +80,7 @@ public class ProcessCodeExecutor implements CodeExecutor {
             return new ExecutionLog("", LogLevelEnum.INFO);
         }
 
-        final var content = encodedLog.split(SEPARATOR);
+        final var content = encodedLog.split(separator);
         final var decodedMessageBytes = Base64.getDecoder().decode(content[0]);
         final var decodedMessage = new String(decodedMessageBytes);
         final var logLevel = LogLevelEnum.fromValue(content[1]).orElse(LogLevelEnum.INFO);
@@ -63,6 +89,6 @@ public class ProcessCodeExecutor implements CodeExecutor {
     }
 
     private List<String> buildRunCommand(String pathToClassDirectory, String outputFilePath, String className) {
-        return List.of("java", "-jar", JAVA_RUNNER_JAR_NAME, pathToClassDirectory, outputFilePath, className);
+        return List.of("java", "-jar", javaRunnerJarName, pathToClassDirectory, outputFilePath, className);
     }
 }
